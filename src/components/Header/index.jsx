d0ec20ce26595556d627b545b1dc3046e86e7a5d
Davi Santos <daviir17@gmail.com>
2024-01-26 00:45:33 -0300

    feat(link style): link do header  estilizado

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,42 +3,19 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-      <div className='header-container'>
-          <div className='header-content'>
-              <div>
-              </div>
-              <div className='home link-page'>
-                <Link className='davi' to='/'><span  className="material-symbols-outlined home-icon">cottage</span></Link>
-              </div>
-              
-              <div className='word link-page'>
-                  <span>Words</span>
-              </div>
-              <div className='text link-page'>
-                  <span>Texts</span>
-              </div>
-              <div className='sentence link-page'>
-                  <span>Sentences</span>
-              </div>
-              <div>
-      
-              </div>
-          </div>
-          <div>
-          </div>
-      </div>
-      )
-    }
-    
-    {/* <nav className='header-container'>
-        <ul>
-            <li></li>
-            <Link to='/'><li className='home link-page'><span className="material-symbols-outlined home-icon">cottage</span></li></Link>
-            <Link><li className='Words link-page'>Words</li></Link>
-            <Link><li className='Texts link-page'>Texts</li></Link>
-            <Link><li className='Sentences link-page'>Sentences</li></Link>
-            <li></li>
-        </ul>
+    <nav className='header-container'>
+      <div className='header-content'>
         <div></div>
-    </nav> */}
+        <Link to='/' className='home link-page'><span className="material-symbols-outlined home-icon">cottage</span></Link>
+        <Link className='word link-page'>Words</Link>
+        <Link className='text link-page'>Texts</Link>
+        <Link className='sentence link-page'>Sentences</Link>
+        <div></div>
+      </div>
+      <div></div>
+    </nav>
+      );
+}
+    
+    
     
