fd3acdbad4c7c570ab68dee5675c9edec39f00d7
Davi Santos <daviir17@gmail.com>
2024-01-27 02:04:33 -0300

    feat(wordpreview page): finalização da imagem e icon de audio

--- src/pages/WordPreview/index.jsx
+++ src/pages/WordPreview/index.jsx
@@ -2,6 +2,7 @@ import Container from '../../components/Container';
 import Input from '../../components/Input';
 import './styles.css';
 
+
 export default function WordPreview() {
   return (
     <div className='wordPreview-container'>
@@ -15,12 +16,16 @@ export default function WordPreview() {
         />
         <Container id={'wordPreview-containerComponent'}>
             <div className='word-image-wordPreview'>
-
+                
                 <span className="material-symbols-outlined icon-audio-wordPreview">volume_up</span>
 
-                <h1 className='word-wordPreview'>Microwave</h1>
+                <h1 className='title-wordPreview'>Microwave</h1>
+                <br />
 
-                <div className='image-wordPreview'><img src="https://i.zst.com.br/thumbs/12/12/2e/-542064200.jpg" alt="microwave" /></div>
+                    <div className='image-wordPreview'>
+                        <img src="https://i.zst.com.br/thumbs/12/12/2e/-542064200.jpg" alt="microwave" />
+                    </div>
+               
 
                 <div className='progressInput-wordPreview'>barra</div>
                 
