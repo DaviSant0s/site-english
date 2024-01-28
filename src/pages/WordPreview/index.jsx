fb7c3ae21570d7c586c0863ca21119089b83ca48
Davi Santos <daviir17@gmail.com>
2024-01-28 18:39:10 -0300

    feat(wordpreview page): finalização da parte da imagem e audio

--- src/pages/WordPreview/index.jsx
+++ src/pages/WordPreview/index.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import Container from '../../components/Container';
 import Input from '../../components/Input';
 import './styles.css';
+import { useEffect } from 'react';
 
 
 export default function WordPreview() {
+    const  [ audio, setAudio ]  = useState(false);
+
+    const handleClickAudio = () => {
+        setAudio(s => !s)
+    }
+
+
   return (
     <div className='wordPreview-container'>
         <Input
@@ -17,17 +26,23 @@ export default function WordPreview() {
         <Container id={'wordPreview-containerComponent'}>
             <div className='word-image-wordPreview'>
                 
-                <span className="material-symbols-outlined icon-audio-wordPreview">volume_up</span>
+                <span onClick={handleClickAudio} className="material-symbols-outlined icon-audio-wordPreview">
+                    {audio ? 'volume_up' : 'stop'}
+                </span>
 
                 <h1 className='title-wordPreview'>Microwave</h1>
-                <br />
 
+                <div className='image-wordPreview-container'>
+                    <span className="material-symbols-outlined arrow-image-wordPreview-left">chevron_left</span>
                     <div className='image-wordPreview'>
+                        {/* <img src="https://www.sofacasa.com.br/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/s/u/sui_a_bordado_138.jpg" alt="microwave" /> */}
                         <img src="https://i.zst.com.br/thumbs/12/12/2e/-542064200.jpg" alt="microwave" />
                     </div>
+                    <span className="material-symbols-outlined arrow-image-wordPreview-right">chevron_right</span>
+                </div>
                
 
-                <div className='progressInput-wordPreview'>barra</div>
+                <div className='progressInput-wordPreview'><input type="range" min='0' max='10'/></div>
                 
             </div>
             <div className='word-sentence-wordPreview'>
