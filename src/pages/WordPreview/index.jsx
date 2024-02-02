8df9b7b2a5de38af1cabb15a61c5dd8f6c3b22e7
Davi Santos <daviir17@gmail.com>
2024-02-02 03:34:31 -0300

    feat(wordpreview page): quase acabada, falta o botão

--- src/pages/WordPreview/index.jsx
+++ src/pages/WordPreview/index.jsx
@@ -3,34 +3,41 @@ import Container from '../../components/Container';
 import Input from '../../components/Input';
 import './styles.css';
 import Loading from '../../components/Loading';
-
+import audioteste from '../../assets/audio.mp3';
 const imagesObject = [
     { 
         id: 1,
         name: 'Microwave',
         link:' https://i.zst.com.br/thumbs/12/12/2e/-542064200.jpg',
+        sentence: 'He became responsible for the design of microwave mixers, needed in most microwave radar systems He became responsible for the design of microwave mixers needed in most microwave radar systems.',
     },
+
+    
     
     {
         id: 2,
         name: 'Bed',
         link: 'https://mysleepyhead.com/media/catalog/product/q/u/queen_size_wooden_bed_1.jpg',
+        sentence: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos consequatur qui saepe hic alias explicabo mollitia culpa exercitationem, unde cumque quod officia voluptate, molestiae eius necessitatibus',
     },
     {
         id: 3,
         name: 'Bicycle',
         link: 'https://www.prioritybicycles.com/cdn/shop/products/600_August_2022_angledfront.jpg?v=1663084384&width=1500',
+        sentence: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos consequatur qui saepe hic alias explicabo mollitia culpa exercitationem, unde cumque quod officia voluptate, molestiae eius necessitatibus',
     },
     {
         id: 4,
         name: 'Car',
         link: 'https://img.freepik.com/fotos-gratis/um-muscle-car-azul-e-preto-com-a-placa-que-diz-trans-na-frente_1340-23399.jpg?size=626&ext=jpg&ga=GA1.1.1880011253.1699142400&semt=ais',
+        sentence: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos consequatur qui saepe hic alias explicabo mollitia culpa exercitationem, unde cumque quod officia voluptate, molestiae eius necessitatibus',
     },
     
     {
         id: 5,
         name: 'House',
         link: 'https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/home-improvement/wp-content/uploads/2022/07/download-23.jpg',
+        sentence: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos consequatur qui saepe hic alias explicabo mollitia culpa exercitationem, unde cumque quod officia voluptate, molestiae eius necessitatibus',
     }
 ];
 
@@ -79,31 +86,34 @@ export default function WordPreview() {
             idContainer={'wordPreviewInput-container'}
         />
         <Container id={'wordPreview-containerComponent'}>
-            <div className='word-image-wordPreview'>
-                
-                <span onClick={handleClickAudio} className='icon-audio-wordPreview'>
-                    <span className="material-symbols-outlined ">
-                        {audio ? 'volume_up' : 'volume_off'}
-                    </span>
+            <span onClick={handleClickAudio} className='icon-audio-wordPreview'>
+                <span className="material-symbols-outlined ">
+                    {audio ? 'volume_up' : 'volume_off'}
                 </span>
+            </span>
+            <div className='wordPreview-content'>
 
-                <h1 className='title-wordPreview'>{imagesObject[indexImage].name}</h1>
-
-                <div className='image-wordPreview-container'>
-                    <span onClick={handleClickPreviousImage} className="material-symbols-outlined arrow-image-wordPreview-left">chevron_left</span>
-                    <div className='image-wordPreview'>
-                        <img src={image} alt="microwave" />
-                        {/* <Loading /> */}
-                    </div>
-                    <span onClick={handleClickNextImage} className="material-symbols-outlined arrow-image-wordPreview-right">chevron_right</span>
-                </div>
-               
-
-                <div className='progressInput-wordPreview'><input type="range" min='1' max={imagesObject.length} value={indexImage + 1}/></div>
                 
-            </div>
-            <div className='word-sentence-wordPreview'>
-                frase
+
+                <div className='word-image-wordPreview'>
+                    <h1 className='title-wordPreview'>{imagesObject[indexImage].name}</h1>
+                    <div className='image-wordPreview-container'>
+                        <span onClick={handleClickPreviousImage} className="material-symbols-outlined arrow-image-wordPreview-left">chevron_left</span>
+                        <div className='image-wordPreview'>
+                            <img src={image} alt="microwave" />
+                            {/* <Loading /> */}
+                        </div>
+                        <span onClick={handleClickNextImage} className="material-symbols-outlined arrow-image-wordPreview-right">chevron_right</span>
+                    </div>
+                    <div className='progressInput-wordPreview'><input type="range" min='1' max={imagesObject.length } value={indexImage + 1}/></div>
+                </div>
+                
+                <div className='word-sentence-wordPreview-container'>
+                    <div className='word-sentence-wordPreview-content'>
+                        <div>{imagesObject[indexImage].sentence}</div>
+                        <audio className='audio-sentence-wordPreview' src={audioteste} controls autoPlay></audio>
+                    </div>
+                </div>
             </div>
         </Container>
     </div>
