cf1a8eecb96b18969ce15433032abf264f5d77b1
Davi Santos <daviir17@gmail.com>
2024-02-13 00:31:05 -0300

    feat(wordpreview): resposividade, ainda falta aguns ajustes finais

--- src/pages/WordPreview/index.jsx
+++ src/pages/WordPreview/index.jsx
@@ -9,7 +9,7 @@ const imagesObject = [
         id: 1,
         name: 'Microwave',
         link: 'https://i.zst.com.br/thumbs/12/12/2e/-542064200.jpg',
-        sentence: 'He became responsible for the design of microwave mixers, needed in most microwave radar systems.',
+        sentence: 'He became responsible for the design of microwave mixers, needed in most microwave radar. Eos consequatur qui saepe hic alias explicabo mollitia culpa exercitationem, unde cumque quod officia voluptate, molestiae eius necessitatibus, He became responsible for the design  of microwave mixers, needed in most microwave radar systems.',
     },
 
     
@@ -72,46 +72,56 @@ export default function WordPreview() {
 
   return (
     <div className='wordPreview-container defaultContainerDimension'>
-        <InputSearch
-            type={'text'}
-            placeholder={'Search'}
-            iconSide='right'
-        />
+
+        <div className='wordPreview-grid'>
+           <InputSearch
+                type={'text'}
+                placeholder={'Search'}
+                iconSide='right'
+                width={'100%'}
+            />
 
         
-        <Container id={'wordPreview-containerComponent'}>
-        <Audio 
-          position={'absolute'} 
-          top={'3px'} 
-          left={'3px'} 
-          fontSize={'1.8em'}
-        />
-            <div className='wordPreview-content'>
+            <Container id={'wordPreview-containerComponent'}>
+                <Audio 
+                    position={'absolute'} 
+                    top={'3px'} 
+                    left={'3px'} 
+                    fontSize={'1.8em'}
+                />
 
-                
+                <div className='wordPreview-content'>
 
-                <div className='word-image-wordPreview'>
-                    <h1 className='title-wordPreview'>{imagesObject[indexImage].name}</h1>
-                    <div className='image-wordPreview-container'>
-                        <span onClick={handleClickPreviousImage} className="material-symbols-outlined arrow-image-wordPreview-left">chevron_left</span>
-                        <div className='image-wordPreview'>
-                            <img src={image} alt="microwave" />
-                            {/* <Loading /> */}
+                    
+
+                    <div className='word-image-wordPreview'>
+                        <h1 className='title-wordPreview'>{imagesObject[indexImage].name}</h1>
+                        <div className='image-wordPreview-container'>
+                            <span onClick={handleClickPreviousImage} className="material-symbols-outlined arrow-image-wordPreview-left">chevron_left</span>
+                            <div className='image-wordPreview'>
+                                <img src={image} alt="microwave" />
+                                {/* <Loading /> */}
+                            </div>
+                            <span onClick={handleClickNextImage} className="material-symbols-outlined arrow-image-wordPreview-right">chevron_right</span>
                         </div>
-                        <span onClick={handleClickNextImage} className="material-symbols-outlined arrow-image-wordPreview-right">chevron_right</span>
+                        <div className='progressInput-wordPreview'><input type="range" min='1' max={imagesObject.length } value={indexImage + 1}/></div>
                     </div>
-                    <div className='progressInput-wordPreview'><input type="range" min='1' max={imagesObject.length } value={indexImage + 1}/></div>
-                </div>
-                
-                <div className='word-sentence-wordPreview-container'>
-                    <div className='word-sentence-wordPreview-content'>
-                        <div>{imagesObject[indexImage].sentence}</div>
-                        <audio className='audio-sentence-wordPreview' src='' controls autoPlay></audio>
-                        <Button id={'ButtonTranslate-wordPreview'} />
+                    
+                    <div className='word-sentence-wordPreview-container'>
+                        <div className='word-sentence-wordPreview-content'>
+                            <div className='word-sentence'>{imagesObject[indexImage].sentence}</div>
+                            <div className='ButtonTranslate-wordPreview-container'>
+                                <Button id={'ButtonTranslate-wordPreview'} />
+                            </div>
+                            <audio className='audio-sentence-wordPreview' src='' controls autoPlay></audio>
+                        </div>
+                        
                     </div>
                 </div>
-            </div>
-        </Container>
+            </Container> 
+        </div>
+
+        
     </div>
   )
 }
